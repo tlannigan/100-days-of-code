4c07e01ea7d52c76a753c1331af43902fbf41ce5
tlannigan <tristanlannigan@gmail.com>
2021-06-09 22:48:09 -0600

    Add Day 3-4 love calculator exercise

---
+++ day-3-control-flow-and-logic/3-4-love-calculator/main.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+combined_name = name1.lower() + name2.lower()
+
+true, love = 0, 0
+
+for i in ["t", "r", "u", "e"]:
+    true += combined_name.count(i)
+
+for i in ["l", "o", "v", "e"]:
+    love += combined_name.count(i)
+
+love_score = int(str(true) + str(love))
+
+if love_score < 10 or love_score > 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif love_score > 40 and love_score < 50:
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
